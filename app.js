2c25474783fc6b22a6d765b9ddd078afa4699381
htkael <htkael@gmail.com>
2025-02-20 10:11:21 -0600

    Wth

--- app.js
+++ app.js
@@ -13,8 +13,9 @@ app.use(express.json());
 app.use(
   cors({
     origin: ["http://localhost:5173", "http://localhost:5174"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+    credentials: true,
   })
 );
 
