9a8b458a7ab49fd93ea4e7342745c7c341a7c60e
htkael <htkael@gmail.com>
2025-02-20 10:19:39 -0600

    Please work

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(
     origin: ["http://localhost:5173", "http://localhost:5174"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 
