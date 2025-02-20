f7725da9acc2240bf7312b61f33851598f61e553
htkael <htkael@gmail.com>
2025-02-20 09:30:48 -0600

    Update cors permissions

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ console.log("Server Start...");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+  })
+);
 
 app.use("/api", indexRouter);
 
