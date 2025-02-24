443c9f8fc8ba6636a28dd3ec2f116ae47a258e5d
htkael <htkael@gmail.com>
2025-02-24 09:06:00 -0600

    CORS fix with deploted site

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["https://hunters-blog-1.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
